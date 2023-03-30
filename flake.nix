22458bbfc6c2b2dd47762292293ac947ce53e8c7
Trouble-Truffle <perigordtruffle7318@gmail.com>
2023-03-30 18:09:41 +0800

    added neovim-nightly as a flake input

--- flake.nix
+++ flake.nix
@@ -4,11 +4,16 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    neovim-nightly-overlay = {
+      url = "github:nix-community/neovim-nightly-overlay";
+      inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=fad51abd42ca17a60fc1d4cb9382e2d79ae31836";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
+    neovim-nightly-overlay,
     flake-utils,
     ...
   }:
@@ -16,7 +21,7 @@
       system: let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [(self: super: {fennel-language-server = super.callPackage ./nix/fennel-language-server.nix {};})];
+          overlays = [(self: super: {fennel-language-server = super.callPackage ./nix/fennel-language-server.nix {};}) neovim-nightly-overlay.overlay];
         };
       in
         with pkgs; {
