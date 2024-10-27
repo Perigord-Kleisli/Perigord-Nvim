f414749821554aaf7ca22b9666a1533ffbe41ed6
Trouble-Truffle <perigordtruffle7318@gmail.com>
2024-10-27 20:38:53 +0800

    add file_ignore_patterns in telescope

--- lua/user/plugins/telescope.lua
+++ lua/user/plugins/telescope.lua
@@ -1,19 +1,21 @@
 return {
   { "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    opts = { defaults = {
-      winblend = 1,
-      prompt_prefix = " ",
-      selection_caret = "  ",
-      mappings = {
-        i = {
-          ["<C-j>"] = require("telescope.actions").move_selection_next,
-          ["<C-k>"] = require("telescope.actions").move_selection_previous,
-          ["<C-n>"] = require("telescope.actions").cycle_history_next,
-          ["<C-p>"] = require("telescope.actions").cycle_history_prev,
+    opts = {
+      defaults = {
+        file_ignore_patterns = { "node_modules", "dist-newstyle", "target", ".git" },
+        winblend = 1,
+        prompt_prefix = " ",
+        selection_caret = "  ",
+        mappings = {
+          i = {
+            ["<C-j>"] = require("telescope.actions").move_selection_next,
+            ["<C-k>"] = require("telescope.actions").move_selection_previous,
+            ["<C-n>"] = require("telescope.actions").cycle_history_next,
+            ["<C-p>"] = require("telescope.actions").cycle_history_prev,
+          }
         }
-      }
-    },
+      },
     }
   }
 }
